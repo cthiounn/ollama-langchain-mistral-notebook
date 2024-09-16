3bb7994f3c40fd5f7c7f39c6bfb5a6c3ae9cfa53
cthiounn2 <thiounn.conrad@gmail.com>
2024-09-16 13:25:03 +0000

    edit script load Cache

--- load_run_ollama.sh
+++ load_run_ollama.sh
@@ -3,7 +3,7 @@ then
     echo "~/.ollama directory exists"
 else
     echo "~/.ollama directory does not exist"
-    cd ~ && mc cp -r s3/$VAULT_TOP_DIR/.ollama .
+    cd ~ && mc cp -r s3/$VAULT_TOP_DIR/Caches/.ollama .
 fi
 ollama serve &
 ollama run mistral-large &
